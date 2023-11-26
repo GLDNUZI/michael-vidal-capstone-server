6db0d09d81edc0c2748ed907d43b741051e400a9
Michael Vidal <vidal@elixrlabs.com>
2023-11-26 16:53:54 -0500

    testing

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: process.env.NODE_ENV === "production" ? { secure: true, maxAge: 30 * 1000 * 60, sameSite: "none", httpOnly: true } : { secure: 'auto', maxAge: 30 * 1000 * 60 }
+    cookie: process.env.NODE_ENV === "production" ? { secure: true, maxAge: 30 * 1000 * 60, sameSite: "None", httpOnly: true } : { secure: 'auto', maxAge: 30 * 1000 * 60 }
 }));
 
 // Initialize Passport
